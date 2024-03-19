22123c22a56124d840e17857a1ba686f84e4dbe5
sky7t <163654567+sky7t@users.noreply.github.com>
2024-03-20 00:33:26 +0530

    Create script.js

---
+++ hope/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var messageElement = document.getElementById("message");
+    messageElement.innerHTML = "Hello, World!";
+});
